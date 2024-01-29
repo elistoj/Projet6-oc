d0b6a260a58125340ccd404d0288c2c5564f44e8
elistoj <atanastojaneli@gmail.com>
2024-01-29 21:48:45 +0100

    Update photographer.js

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,33 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
 
+function photographerTemplate(data) {
+    const { id, name, tagline, city, country, price, portrait } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name); // Ajout d'un attribut alt pour l'accessibilité
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+
+// Ajout d'informations supplémentaires à la carte utilisateur        
+       
+        const cityCountryParagraph = document.createElement('p');
+        cityCountryParagraph.textContent = ` ${city},  ${country}`;
+        const taglineParagraph = document.createElement('p');
+        taglineParagraph.textContent = ` ${tagline}`;
+        const priceParagraph = document.createElement('p');
+        priceParagraph.textContent = ` $${price}/jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(cityCountryParagraph);
+        article.appendChild(taglineParagraph);
+        article.appendChild(priceParagraph);
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    return { id, name, picture, getUserCardDOM };
 }
