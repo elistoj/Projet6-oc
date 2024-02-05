0507772b7e4b475ebaf16a840c535d9a7a284479
elistoj <atanastojaneli@gmail.com>
2024-02-05 15:39:02 +0100

    Update photographer.js

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,9 +1,13 @@
-
+// Fonction pour créer un modèle de photographe
 function photographerTemplate(data) {
+    // Extraction des données du photographe
     const { id, name, tagline, city, country, price, portrait } = data;
+    // Chemin de l'image du photographe
     const picture = `assets/photographers/${portrait}`;
-    
+
+    // Fonction pour obtenir le DOM de la carte utilisateur
     function getUserCardDOM() {
+        // Création des éléments HTML
         const article = document.createElement('article');
         const img = document.createElement('img');
         const figure = document.createElement('figure');
@@ -12,18 +16,18 @@ function photographerTemplate(data) {
         const h2 = document.createElement('h2');
         h2.textContent = name;
 
-// Ajout d'informations supplémentaires à la carte utilisateur        
-       
+        // Ajout d'informations supplémentaires à la carte utilisateur
         const cityCountryParagraph = document.createElement('p');
-        cityCountryParagraph.textContent = ` ${city},  ${country}`;
-        cityCountryParagraph.classList.add('city-country'); // Ajout une class
+        cityCountryParagraph.textContent = ` ${city}, ${country}`;
+        cityCountryParagraph.classList.add('city-country'); // Ajout d'une classe
         const taglineParagraph = document.createElement('p');
-        taglineParagraph.textContent = ` ${tagline}`;   
-        taglineParagraph.classList.add('tagline'); // Ajout une class
+        taglineParagraph.textContent = ` ${tagline}`;
+        taglineParagraph.classList.add('tagline'); // Ajout d'une classe
         const priceParagraph = document.createElement('p');
         priceParagraph.textContent = ` ${price}€/jour`;
-        priceParagraph.classList.add('price'); // Ajout une class
+        priceParagraph.classList.add('price'); // Ajout d'une classe
 
+        // Construction de la structure DOM de la carte utilisateur
         figure.appendChild(img);
         article.appendChild(figure);
         article.appendChild(h2);
@@ -34,5 +38,6 @@ function photographerTemplate(data) {
         return article;
     }
 
+    // Retourne un objet avec les données du photographe et la fonction pour obtenir le DOM de la carte utilisateur
     return { id, name, picture, getUserCardDOM };
 }
